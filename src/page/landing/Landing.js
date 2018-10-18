2de30c1a1af63464bdc1815477c2fca3287a20ab
WingChhun <capt.wing.chhun@gmail.com>
2018-10-18 12:16:15 -0700

    Add react router dom to App component

--- src/page/landing/Landing.js
+++ src/page/landing/Landing.js
@@ -3,6 +3,14 @@ import PropTypes from 'prop-types';
 import classnames from 'classnames';
 import {withStyles} from '@material-ui/core';
 
+// ! - Import Components
+import NavbarComponent from "../../components/Navbar/Navbar.js";
+import HeroComponent from "../../components/Hero/Hero.js";
+import AboutComponent from "../../components/About/About.js";
+import ProjectComponent from "../../components/Project/Project.js";
+import SkillComponent from "../../components/Skill/Skill.js";
+import FooterComponent from "../../components/Footer/Footer.js";
+
 const styles = () => ({root: {}})
 
 class Landing extends Component
