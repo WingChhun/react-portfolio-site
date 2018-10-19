075a10f98fa5e98ca0188be82978935d06b03af6
WingChhun <capt.wing.chhun@gmail.com>
2018-10-18 17:40:59 -0700

    Added reusable media queries from old website: Added typography that scales rem

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -2,6 +2,9 @@ import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
 import {withStyles} from '@material-ui/core';
+
+import Grid from "@material-ui/core/Grid";
+
 const styles = () => ({root: {}})
 
 class Footer extends Component
@@ -20,30 +23,42 @@ class Footer extends Component
         return (
 
             <footer class="footer">
-            <a href="#home" class="footer__arrow">
-                <i class="fa fa-angle-double-up"></i>
-            </a>
-            <div class="footer__container">
-    
-                <a target="blank" href="https://www.linkedin.com/in/james-chhun-16b1b5120/" class="footer__logo-img footer__logo-img--1">
-                    <i class="fa fa-linkedin-square"></i>
+                <a href="#home" class="footer__arrow">
+                    <i class="fa fa-angle-double-up"></i>
                 </a>
-                <a target="blank" href="https://github.com/WingChhun" class="footer__logo-img footer__logo-img--2">
-                    <i class="fa fa-github-square"></i>
-                </a>
-                <a target="blank" href="https://codepen.io/WingChhun/" class="footer__logo-img footer__logo-img--3">
-                    <i class="fa fa-codepen"></i>
-                </a>
-                <a target="blank" href="https://www.instagram.com/wingchhun_codes/" class="footer__logo-img footer__logo-img--4">
-                    <i class="fa fa-instagram"></i>
-                </a>
-    
-                <p class="footer__copyright">Designed and Developed by James Chhun, me &copy; 2018</p>
-    
-            </div>
-            
-    
-        </footer>
+                <div class="footer__container">
+             
+                        <a
+                            target="blank"
+                            href="https://www.linkedin.com/in/james-chhun-16b1b5120/"
+                            class="footer__logo-img footer__logo-img--1">
+                            <i class="fa fa-linkedin-square"></i>
+                        </a>
+                        <a
+                            target="blank"
+                            href="https://github.com/WingChhun"
+                            class="footer__logo-img footer__logo-img--2">
+                            <i class="fa fa-github-square"></i>
+                        </a>
+                        <a
+                            target="blank"
+                            href="https://codepen.io/WingChhun/"
+                            class="footer__logo-img footer__logo-img--3">
+                            <i class="fa fa-codepen"></i>
+                        </a>
+                        <a
+                            target="blank"
+                            href="https://www.instagram.com/wingchhun_codes/"
+                            class="footer__logo-img footer__logo-img--4">
+                            <i class="fa fa-instagram"></i>
+                        </a>
+
+
+                        <p class="footer__copyright">Designed and Developed by James Chhun, me &copy; 2018</p>
+               
+                </div>
+
+            </footer>
         )
     }
 };
