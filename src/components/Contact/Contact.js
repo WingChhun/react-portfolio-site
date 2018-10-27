0514ed6cc5384c513165aa54e9c3384c384972b3
WingChhun <capt.wing.chhun@gmail.com>
2018-10-26 17:15:30 -0700

    Make contact form

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -37,15 +37,15 @@ class Contact extends Component
 
         switch (field) {
             case NAME:
-                this.setState({NAME: value});
+                this.setState({name: value});
                 break;
 
             case EMAIL:
-                this.setState({EMAIL: value});
+                this.setState({email: value});
                 break;
 
             case MESSAGE:
-                this.setState({NAME: messageValue});
+                this.setState({message: messageValue});
                 break;
 
             default:
@@ -58,30 +58,97 @@ class Contact extends Component
 
     submitForm = () => {
 
-        const {name, email} = this.state;
+        const URL = "https://formspree.io/capt.wing.chhun@gmail.com";
+        const {name, email, message} = this.state;
         const formData = new FormData();
 
         //TODO: Validate email
 
-        form.append('name', name);
-        form.append('email', email);
+        formData.set('name', name);
+        formData.set('email', email);
+        formData.set('message', message);
+
+        const configurations = {
+            method: 'post',
+            url: URL,
+            data: formData
+        };
+
+        axios(configurations)
+        //! Working
+            .then(res => {
+            console.log(res);
+            debugger;
+        })
+        //! Fail
+            .catch(err => {
+            console.log(err);
+            debugger;
+        })
     }
 
     render()
     {
 
         const {classes} = this.props;
+        const {name, email, message, loading, errors} = this.state;
 
         return (
 
             <div className={classNames(classes.root, "contact")}>
                 <div className={"contact__container"}>
                     <Grid container justify='center' direction="column">
-                        <h2>
-                            Have a question?</h2>
-                        <p>
-                            Feel free to send me an email</p>
+                        <Grid xs={12}>
+                            <h2>
+                                Have a question?</h2>
+                        </Grid>
+                        <Grid xs={12}>
+                            <p>
+                                Feel free to send me an email</p>
+                        </Grid>
+
                     </Grid>
+
+                    <div className="contact__form">
+                        <Grid container spacing={8} justify='space-around'>
+
+                            <Grid xs={12} md={6}>
+                                <TextField
+                                    value={name}
+                                    name={NAME}
+                                    label='Name'
+                                    onChange={this.handleChange(NAME)}
+                                    fullWidth
+                                    margin="normal"
+                                    variant={'outlined'}/>
+                            </Grid>
+
+                            <Grid xs={12} md={6}>
+                                <TextField
+                                    value={email}
+                                    name={EMAIL}
+                                    label='Email'
+                                    onChange={this.handleChange(EMAIL)}
+                                    fullWidth
+                                    margin="normal"
+                                    variant={'outlined'}/>
+                            </Grid>
+
+                            <Grid xs={12}>
+                                <TextField
+                                    value={message}
+                                    name={MESSAGE}
+                                    label='Message'
+                                    onChange={this.handleChange(MESSAGE)}
+                                    fullWidth
+                                    margin="normal"
+                                    variant={'outlined'}
+                                    multiline
+                                    rowsMax="4"
+                                    rows='4'/>
+                            </Grid>
+                        </Grid>
+                    </div>
                 </div>
             </div>
 
