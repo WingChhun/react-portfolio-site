45008303f58c9db2ba97d4f80e7bab4458b330e2
WingChhun <capt.wing.chhun@gmail.com>
2018-11-22 13:21:03 -0800

    Fix links add more assets

--- src/assets/portfolio/images/navismart/index.js
+++ src/assets/portfolio/images/navismart/index.js
@@ -4,8 +4,28 @@ import navismart_3 from "./navismart_3.png";
 import navismart_4 from "./navismart_4.png";
 import navismart_5 from "./navismart_5.png";
 import navismart_6 from "./navismart_6.png";
-
+import About from "./photos/About.png";
+import contact from "./photos/contact.png";
+import Landing_Hero from "./photos/Landing_Hero.png";
+import Product__create from "./photos/Product__create.png";
+import Product__page from "./photos/Product__page.png";
+import product__phone from "./photos/product__phone.png";
+import Product_hero from "./photos/Product_hero.png";
+import Product_signu0p from "./photos/Product_signu0p.png";
+import system from "./photos/system";
+import Sequence from "./photos/Sequence";
 export default {
+
+    About,
+    system,
+    contact,
+    Landing_Hero,
+    Product__create,
+    Product__page,
+    product__phone,
+    Product_hero,
+    Product_signu0p,
+    Sequence,
     navismart_1,
     navismart_2,
     navismart_3,
