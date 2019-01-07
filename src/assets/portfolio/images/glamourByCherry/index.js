f250228cd7f45a31467620f4c8bce046fdef1021
WingChhun <capt.wing.chhun@gmail.com>
2019-01-07 11:19:57 -0800

    Add assets glamour

--- src/assets/portfolio/images/glamourByCherry/index.js
+++ src/assets/portfolio/images/glamourByCherry/index.js
@@ -1,5 +1,12 @@
 import glamour_1 from "./glamour_1.png";
-
+import about from "./about.png";
+import gallery from "./gallery.png";
+import instagram from "./instagram.png";
+import testimonials from "./testimonials.png";
 export default {
-    glamour_1
+    glamour_1,
+    about,
+    gallery,
+    instagram,
+    testimonials
 }
