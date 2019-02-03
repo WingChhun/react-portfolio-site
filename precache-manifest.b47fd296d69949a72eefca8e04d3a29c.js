3284a651bdfe49ab83780cfe5849510443d5e3bf
James Chhun <capt.wing.chhun@gmail.com>
2019-02-03 14:58:58 -0800

    Updates

---
+++ precache-manifest.b47fd296d69949a72eefca8e04d3a29c.js
@@ -0,0 +1,278 @@
+self.__precacheManifest = [
+  {
+    "revision": "63a63cf0825937e42a42",
+    "url": "/react-portfolio-site/static/css/main.dd3eea7a.chunk.css"
+  },
+  {
+    "revision": "63a63cf0825937e42a42",
+    "url": "/react-portfolio-site/static/js/main.63a63cf0.chunk.js"
+  },
+  {
+    "revision": "c283d5e1452e4f47b030",
+    "url": "/react-portfolio-site/static/js/1.c283d5e1.chunk.js"
+  },
+  {
+    "revision": "8499128c9c0f9eb496fc",
+    "url": "/react-portfolio-site/static/js/runtime~main.8499128c.js"
+  },
+  {
+    "revision": "c75e64615fa6f6b9d460923a2c63df05",
+    "url": "/react-portfolio-site/static/media/video-2.c75e6461.mp4"
+  },
+  {
+    "revision": "000648b746720f2c776a80749c66502a",
+    "url": "/react-portfolio-site/static/media/JamesChhun_Resume_updated_4.000648b7.pdf"
+  },
+  {
+    "revision": "65e55ff5070d693b8f974fa7928084bd",
+    "url": "/react-portfolio-site/static/media/jest.65e55ff5.png"
+  },
+  {
+    "revision": "68b835f86d9402568aa41a2946798246",
+    "url": "/react-portfolio-site/static/media/react.68b835f8.png"
+  },
+  {
+    "revision": "4912f6d800e1b75354e0b5d4794e1856",
+    "url": "/react-portfolio-site/static/media/Redux.4912f6d8.png"
+  },
+  {
+    "revision": "3b96e32f2c895e87be2fd41ebed43c1b",
+    "url": "/react-portfolio-site/static/media/cherry_1.3b96e32f.png"
+  },
+  {
+    "revision": "b0787d09aeeda270bf9428f0da736a57",
+    "url": "/react-portfolio-site/static/media/cherry_2.b0787d09.png"
+  },
+  {
+    "revision": "366a0fa1972a00271e700e884a7eab7d",
+    "url": "/react-portfolio-site/static/media/cherry_3.366a0fa1.png"
+  },
+  {
+    "revision": "c4540decf8caa3695b4eabb49a1f222a",
+    "url": "/react-portfolio-site/static/media/cherry_4.c4540dec.png"
+  },
+  {
+    "revision": "c3ce604230513705550a80e099fc93e9",
+    "url": "/react-portfolio-site/static/media/cherry_5.c3ce6042.png"
+  },
+  {
+    "revision": "45a7d56fb3937b079e2b072bc7859da2",
+    "url": "/react-portfolio-site/static/media/cherry_6.45a7d56f.png"
+  },
+  {
+    "revision": "5dc48da71ba9f412c22b0cf34e91d5f9",
+    "url": "/react-portfolio-site/static/media/card_1.5dc48da7.png"
+  },
+  {
+    "revision": "d0420bcb42362555a28209fcfdb26f4c",
+    "url": "/react-portfolio-site/static/media/card_2.d0420bcb.png"
+  },
+  {
+    "revision": "88a9ef7c5e5294213017986d1400080d",
+    "url": "/react-portfolio-site/static/media/react_card_1.88a9ef7c.png"
+  },
+  {
+    "revision": "3e9a179921c21b6c3779b44637dcf3b2",
+    "url": "/react-portfolio-site/static/media/portfolio_1.3e9a1799.png"
+  },
+  {
+    "revision": "8ad23a90ef0019f6a40cc955588decec",
+    "url": "/react-portfolio-site/static/media/portfolio_2.8ad23a90.png"
+  },
+  {
+    "revision": "d8321156ac5f54b07320646c24cb7e64",
+    "url": "/react-portfolio-site/static/media/old_portfolio_1.d8321156.png"
+  },
+  {
+    "revision": "486011e1e9e0c463579c25c898986998",
+    "url": "/react-portfolio-site/static/media/old_portfolio_2.486011e1.png"
+  },
+  {
+    "revision": "03643dd4e1e48e7e5c121f2f607f2af0",
+    "url": "/react-portfolio-site/static/media/old_portfolio_3.03643dd4.png"
+  },
+  {
+    "revision": "8277e3238cc20180a8291f7e623ab195",
+    "url": "/react-portfolio-site/static/media/modern_portfolio_1.8277e323.png"
+  },
+  {
+    "revision": "a5c81a5d354719fdaad410f5f27fcc43",
+    "url": "/react-portfolio-site/static/media/modern_portfolio_2.a5c81a5d.png"
+  },
+  {
+    "revision": "223247ebf59d043247e99e865452e75d",
+    "url": "/react-portfolio-site/static/media/modern_portfolio_3.223247eb.png"
+  },
+  {
+    "revision": "56188a91a7bd4bf8d3a06f62114a2d3b",
+    "url": "/react-portfolio-site/static/media/modern_portfolio_4.56188a91.png"
+  },
+  {
+    "revision": "d95d151f53a5fead1f6735f3be4fed76",
+    "url": "/react-portfolio-site/static/media/modern_portfolio_5.d95d151f.png"
+  },
+  {
+    "revision": "3c4b5330425827ec1900c4308aecfceb",
+    "url": "/react-portfolio-site/static/media/natour_1.3c4b5330.png"
+  },
+  {
+    "revision": "f8835b639289858f9e98ccf008f008ba",
+    "url": "/react-portfolio-site/static/media/natour_2.f8835b63.png"
+  },
+  {
+    "revision": "4afe7532eeaeabd067d97bdc7b6b0195",
+    "url": "/react-portfolio-site/static/media/natour_3.4afe7532.png"
+  },
+  {
+    "revision": "da52fe0f9f60a36c13c04897ee89d13f",
+    "url": "/react-portfolio-site/static/media/natour_4.da52fe0f.png"
+  },
+  {
+    "revision": "a22533bef4e718160309eca2c97599b6",
+    "url": "/react-portfolio-site/static/media/natour_5.a22533be.png"
+  },
+  {
+    "revision": "1a42911170f26c65e5c6a7d713f73765",
+    "url": "/react-portfolio-site/static/media/orange_rabbit_1.1a429111.png"
+  },
+  {
+    "revision": "626e1f478c7ec3280508e4700c80a6a3",
+    "url": "/react-portfolio-site/static/media/orange_rabbit_2.626e1f47.png"
+  },
+  {
+    "revision": "2e82c4d01a217e3496eff4ecd1bb057e",
+    "url": "/react-portfolio-site/static/media/pixabay_1.2e82c4d0.png"
+  },
+  {
+    "revision": "91270457649780afb95bc4bea29071eb",
+    "url": "/react-portfolio-site/static/media/pixabay_2.91270457.png"
+  },
+  {
+    "revision": "1de0d655053820a2c5cd8bae775b8002",
+    "url": "/react-portfolio-site/static/media/rick_morty_1.1de0d655.png"
+  },
+  {
+    "revision": "c9066bc8937c67358baee775197a102b",
+    "url": "/react-portfolio-site/static/media/rick_morty_2.c9066bc8.png"
+  },
+  {
+    "revision": "e24a1ccdcd832786665c86e1d6cbed88",
+    "url": "/react-portfolio-site/static/media/redux_lumdb_1.e24a1ccd.png"
+  },
+  {
+    "revision": "f87690c215c9421d957b4f5b479c85d3",
+    "url": "/react-portfolio-site/static/media/redux_lumdb_2.f87690c2.png"
+  },
+  {
+    "revision": "3a0fcfb8cf2c34d862655f7994673a75",
+    "url": "/react-portfolio-site/static/media/about.3a0fcfb8.png"
+  },
+  {
+    "revision": "9ddcf5e5e00ab1df6cb6757536f66d08",
+    "url": "/react-portfolio-site/static/media/gallery.9ddcf5e5.png"
+  },
+  {
+    "revision": "bbfce38fb0435572f44c92d7b04072c6",
+    "url": "/react-portfolio-site/static/media/testimonials.bbfce38f.png"
+  },
+  {
+    "revision": "3465d9dcca9b126b1f580c987f0da8f5",
+    "url": "/react-portfolio-site/static/media/hero_desktop.3465d9dc.png"
+  },
+  {
+    "revision": "8f021080f7b0941b1549cb5cfa595c38",
+    "url": "/react-portfolio-site/static/media/hero_iphone.8f021080.png"
+  },
+  {
+    "revision": "432515008b8fee20c0b02bd096603289",
+    "url": "/react-portfolio-site/static/media/instagram_desktop.43251500.png"
+  },
+  {
+    "revision": "b833fc1763b2c74ec674eb988181e19b",
+    "url": "/react-portfolio-site/static/media/navbar_iphone.b833fc17.png"
+  },
+  {
+    "revision": "46fe7ef0ab7197d39e9b72ac78c0b478",
+    "url": "/react-portfolio-site/static/media/services_desktop.46fe7ef0.png"
+  },
+  {
+    "revision": "488e97ba11a93952b132ac3f96473878",
+    "url": "/react-portfolio-site/static/media/navismart_1.488e97ba.png"
+  },
+  {
+    "revision": "7bd69b3f9e39274c43255f36942589fa",
+    "url": "/react-portfolio-site/static/media/navismart_2.7bd69b3f.png"
+  },
+  {
+    "revision": "adf9bed80db561351e3728eb1ca94dff",
+    "url": "/react-portfolio-site/static/media/navismart_3.adf9bed8.png"
+  },
+  {
+    "revision": "6a1609531e7206fb90c94ad010ac2e25",
+    "url": "/react-portfolio-site/static/media/navismart_4.6a160953.png"
+  },
+  {
+    "revision": "aca571b582262e2a5d7724ec222d7078",
+    "url": "/react-portfolio-site/static/media/navismart_5.aca571b5.png"
+  },
+  {
+    "revision": "793280488b4146a3dae3695fff97020e",
+    "url": "/react-portfolio-site/static/media/navismart_6.79328048.png"
+  },
+  {
+    "revision": "617ae1b9fc81659bc6846b457660ad1e",
+    "url": "/react-portfolio-site/static/media/About.617ae1b9.png"
+  },
+  {
+    "revision": "cfd62f8c77fe97695e5784a755beddfe",
+    "url": "/react-portfolio-site/static/media/contact.cfd62f8c.png"
+  },
+  {
+    "revision": "002e145ba9513957f67ff7f16cdf2ef5",
+    "url": "/react-portfolio-site/static/media/Landing_Hero.002e145b.png"
+  },
+  {
+    "revision": "6e37eff6ab8a1f857a6d86f29680d4b2",
+    "url": "/react-portfolio-site/static/media/Product__create.6e37eff6.png"
+  },
+  {
+    "revision": "dc34bb4367abe845cb68d2f0cee8f015",
+    "url": "/react-portfolio-site/static/media/Product__page.dc34bb43.png"
+  },
+  {
+    "revision": "8797111225b2e5f6d90fe02a58a65a2b",
+    "url": "/react-portfolio-site/static/media/product__phone.87971112.png"
+  },
+  {
+    "revision": "df7e31021850c9d5f12eb92c1d596ea5",
+    "url": "/react-portfolio-site/static/media/Product_hero.df7e3102.png"
+  },
+  {
+    "revision": "36b69f29a7e678bcb7569ecff51a86c9",
+    "url": "/react-portfolio-site/static/media/Product_signu0p.36b69f29.png"
+  },
+  {
+    "revision": "2e2be2831735684c3f51cede51f803fa",
+    "url": "/react-portfolio-site/static/media/system.2e2be283.png"
+  },
+  {
+    "revision": "cdc7fcea566e01bdd81e8dacc4187c72",
+    "url": "/react-portfolio-site/static/media/Sequence.cdc7fcea.png"
+  },
+  {
+    "revision": "3763621870976f073715f834731902c9",
+    "url": "/react-portfolio-site/static/media/glamour_1.37636218.png"
+  },
+  {
+    "revision": "c73abd845ddade89acce6b81b0769824",
+    "url": "/react-portfolio-site/static/media/footer_desktop.c73abd84.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-portfolio-site/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "69cf5c3758b728171fa667412629458b",
+    "url": "/react-portfolio-site/index.html"
+  }
+];
